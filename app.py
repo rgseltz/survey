ed406fd35e2f8564f93c655b9a953b4b68d0ff1a
ryan <ryanjgecelter@gmail.com>
2022-05-06 10:12:53 +0300

    improved functionality and created logic based on session cookies. also used session to store user data on server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, render_template, request, redirect, jsonify
+from flask import Flask, flash, render_template, request, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,7 +8,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=True
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+
 @app.route('/')
 def show_starting_page():
     title = satisfaction_survey.title
@@ -17,17 +17,27 @@ def show_starting_page():
 
 @app.route('/begin', methods=['POST'])
 def begin_survey():
+    session['RESPONSES_KEY'] = [] 
     return redirect('question/0')
 
 @app.route('/question/<int:id>')
 def show_question(id):
+    responses = session.get('RESPONSES_KEY', False)
     questions = satisfaction_survey.questions[id]
-    return render_template('question.html', question=questions)
+    if len(responses)!= id:
+        flash('Please do not attempt to answer the questions out of order')
+        return redirect(f'/question/{len(responses)}')
+    
+    return render_template('question.html', question=questions, id=id)
 
 @app.route('/answer', methods=['POST'])
-def submit_answer_1():   
-    return redirect("/question/2")
+def submit_answer():
+    choice = request.form['answer']
+    responses = session['RESPONSES_KEY']
+    responses.append(choice)
+    session['RESPONSES_KEY'] = responses
+    if len(responses) == len(satisfaction_survey.questions):
+        return '<h1>Thank you for completing this survey</h1>'
+    else:
+        return redirect(f'question/{len(responses)}')
 
-@app.route('/answer', methods=['POST'])
-def submit_answer_2():    
-    return redirect("/question/3")
